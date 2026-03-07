da2f2a13f047ef5a7aa6257e903f4f2e4981df87
ahmedyasser002 <ay012941@gmail.com>
2026-03-07 23:33:52 +0200

    Handling Errors using AsyncHandler , Global Error Handler , Custom Error Class , Modifying App Structure

---
+++ Middlewares/globalError.js
@@ -0,0 +1,8 @@
+
+
+export const globalError = (err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+};
