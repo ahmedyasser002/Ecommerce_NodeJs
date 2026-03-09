415d8a248d823b9bb293930f9deff244012cab39
alaamagdy20211 <alaamagdy3008@gmail.com>
2026-03-09 22:55:25 +0200

    email confirmation

--- Routes/auth.route.js
+++ Routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { checkEmail } from "../Middlewares/checkEmail.js";
-import { signup ,signin } from "../Controllers/auth.controller.js";
+import { signup ,signin, emailVerification } from "../Controllers/auth.controller.js";
 import { validationforRegisteation ,validationforLogin } from "../Middlewares/validation.middleware.js";
 import { upload } from "../Utils/upload.js";
 let authRoutes = express.Router();
 authRoutes.post("/signup",upload.single('image'),validationforRegisteation,checkEmail,signup)
 authRoutes.post("/signin" ,validationforLogin,checkEmail ,signin )
+authRoutes.get("/verify/:email", emailVerification)
+
 export default authRoutes
