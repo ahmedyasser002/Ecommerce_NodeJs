ded49d18c04afe9fd303bc108cff080ac5959e12
alaamagdy20211 <alaamagdy3008@gmail.com>
2026-03-09 23:42:25 +0200

    solve confilcts

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,14 +1,30 @@
-import { userModel } from "../Models/User";
+import { userModel } from "../Models/User.js";
 
 let list_users = async (req,res)=>{
     let users = await userModel.find()
-    res.json({mesaage: "List of users", data: users});
-}
+    res.status(200).json({message: "Users retrieved successfully",count: users.length,dta: users
+    });}
 
 let deleteUser =async (req,res) => {
     let id = req.params.id
     const deleteUser = await userModel.findByIdAndDelete(id)
-    res.json({message: "user deleted"})
+     if (!deleteUser) {
+      return res.status(404).json({message: "User not found"});
+    }
+
+    res.status(200).json({
+      message: "User deleted successfully",data: deleteUser});
  }
 
+ let updateUser = async(req,res)=>{
+     let id = req.params.id
+     const updatedUser = await userModel.findByIdAndUpdate(id,req.body,{ new: true, runValidators: true });
+
+    if (!updatedUser) {
+      return res.status(404).json({message: "User not found"});
+    }
+        res.status(200).json({message: "User updated successfully",data: updatedUser});
+ }
+
+ export {list_users, updateUser, deleteUser}
  
--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.config.js";
 import authRoutes from "./Routes/auth.route.js";
+import productRoutes from "./Routes/product.route.js";
+import reviewRoutes from "./Routes/review.route.js";
+import userRoutes from "./Routes/user.route.js";
 
 dotenv.config();
 
@@ -13,6 +16,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(authRoutes)
+
+app.use("/api/products", productRoutes);
+app.use(reviewRoutes)
+app.use(userRoutes)
 
 app.get("/", (req, res) => {
   res.send("Ecommerce API Running");
