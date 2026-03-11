8eebe4e17486e21ad6436e1cbac3e00d29d1efe2
alaamagdy20211 <alaamagdy3008@gmail.com>
2026-03-11 20:52:08 +0200

    fix middleware filename casing

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.config.js";
 import authRoutes from "./Routes/auth.route.js";
-import productRoutes from "./Routes/product.route.js";
-import reviewRoutes from "./Routes/review.route.js";
-import cartRoutes from "./Routes/cart.route.js";
 import userRoutes from "./Routes/user.route.js";
 
 dotenv.config();
@@ -17,11 +14,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use("/category", categoryRoutes)
 app.use("/auth", authRoutes);
-app.use("/products", productRoutes);
-app.use("/reviews", reviewRoutes);
-app.use("/cart", cartRoutes);
 app.use("/user", userRoutes);
 
 
