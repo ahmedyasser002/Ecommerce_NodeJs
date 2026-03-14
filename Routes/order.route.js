784e3b9eee1909f1311e27aea21756fdd32a1077
ahmedyasser002 <ay012941@gmail.com>
2026-03-15 01:42:48 +0200

    Adding update order , cancel order , creating middleware to check the order access

--- Routes/order.route.js
+++ Routes/order.route.js
@@ -2,9 +2,11 @@ import express from "express";
 import { isauthenticated } from "../Middlewares/authenticationMiddleware.js";
 import validationMiddleware from "../Middlewares/ValidationMiddleWare.js";
 import { createOrderSchema } from "../Validations/createOrderValidation.js";
-import { createOrder, getAdminOrders, getCustomerOrders, getOrderById, getSellerOrders } from "../Controllers/order.controller.js";
+import { cancelOrder, createOrder, getAdminOrders, getCustomerOrders, getOrderById, getSellerOrders, updateOrder } from "../Controllers/order.controller.js";
 import { ROLES } from "../Constants/roles.js";
 import { authorizationMiddleware } from "../Middlewares/autorizationMiddleware.js";
+import { productUpdateSchema } from "../Validations/productValidation.js";
+import { checkOrderAccess } from "../Middlewares/checkOrderAccess.js";
 
 
 
@@ -17,6 +19,9 @@ orderRoutes.get("/customer", isauthenticated,authorizationMiddleware(ROLES.CUSTO
 orderRoutes.get("/seller", isauthenticated, authorizationMiddleware(ROLES.SELLER), getSellerOrders)
 orderRoutes.get("/admin", isauthenticated, authorizationMiddleware(ROLES.ADMIN), getAdminOrders)
 orderRoutes.get("/admin/:id", isauthenticated, authorizationMiddleware(ROLES.ADMIN), getOrderById)
+orderRoutes.patch("/update/:id", isauthenticated, authorizationMiddleware(ROLES.ADMIN),validationMiddleware(productUpdateSchema), updateOrder)
+orderRoutes.patch("/cancel/:id",isauthenticated,authorizationMiddleware(ROLES.CUSTOMER,ROLES.ADMIN),checkOrderAccess,cancelOrder)
+
 
 
 
