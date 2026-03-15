54888f774cae2a960bb7103207c4df1448dff33a
alaamagdy20211 <alaamagdy3008@gmail.com>
2026-03-15 15:49:58 +0200

    enhance authentication feature

---
+++ Config/passport.config.js
@@ -0,0 +1,48 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { userModel } from "../Models/User.js";
+
+export const setupPassport = () => {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:5000/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const email = profile.emails[0].value;
+          let user = await userModel.findOne({ email });
+
+          if (!user) {
+            user = await userModel.create({
+              name: profile.displayName,
+              email: email,
+              password: "google-login",
+              isConfirmed: true,
+              image: profile.photos[0].value,
+              googleId: profile.id,
+              address: { country: "Unknown", city: "Unknown", street: "Unknown" } 
+            });
+          } else {
+            user.name = profile.displayName;
+            user.image = profile.photos[0].value;
+            user.googleId = profile.id;
+            await user.save();
+          }
+
+          return done(null, user);
+        } catch (error) {
+          return done(error, null);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => done(null, user._id));
+  passport.deserializeUser(async (id, done) => {
+    const user = await userModel.findById(id);
+    done(null, user);
+  });
+};
--- Middlewares/authenticationMiddleware.js
+++ Middlewares/authenticationMiddleware.js
@@ -1,17 +1,46 @@
 import jwt from "jsonwebtoken" ;
-export let isauthenticated = (req,res,next) => {
-     let token = req.headers.token
-     const secret_key = process.env.JWT_SECRET ;
-    if(!token){
-     return   res.status(401).json({message:"no token provided"})
-    }
-      jwt.verify(token, secret_key, async(err, decoded) => {
-           if(err){
-               return res.status(401).json({message: "Invalid Token"})
-           }else{
-                req.user = decoded
-               next();
-           }
-       })
-}
+export const isauthenticated = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const secret_key = process.env.JWT_SECRET;
 
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, secret_key, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid Token" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+
+
+export const guestOrUserAuthentication = async (req, res, next) => {
+      const authHeader = req.headers.authorization;
+      const secret_key = process.env.JWT_SECRET;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return next(); // guest user
+    }
+
+
+    jwt.verify(token, secret_key, async(err, decoded) => {
+        if(err){
+            return res.status(401).json({message: "Invalid Token"})
+        }else{
+            req.user = decoded
+            next();
+        }
+    })
+};
