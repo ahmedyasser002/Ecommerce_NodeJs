2678b391158476a9279f9184bb75f1a8867900c8
Mustafa Tarek <mustafatarek104@gmail.com>
2026-03-16 06:31:07 +0200

    Merge guest cart after sign in

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -1,32 +1,31 @@
-import { userModel } from "../Models/User.js" ;
-import jwt from "jsonwebtoken" ;
+import { userModel } from "../Models/User.js";
+import jwt from "jsonwebtoken";
 import fs from "fs";
 import path from "path";
 import bcrypt from "bcrypt";
-import asyncHandler from "../Middlewares/asyncHandler.js"
+import asyncHandler from "../Middlewares/asyncHandler.js";
 import { fileURLToPath } from "url";
 import { sendEmail } from "../Email/sendEmail.js";
+import Cart from "../Models/Cart.js"
 
-
-
-let signup = asyncHandler( async (req , res)=>{
-    if (req.file) {
-        req.body.image = req.file.filename;
-    }
-    let newUser = await userModel.create(req.body);
-    sendEmail(req.body.email)
-    newUser.password  = undefined ;
-    res.status(201).json({message:"user created succsefully" , payload : newUser})
-
-}
-)
-
+let signup = asyncHandler(async (req, res) => {
+  if (req.file) {
+    req.body.image = req.file.filename;
+  }
+  let newUser = await userModel.create(req.body);
+  sendEmail(req.body.email);
+  newUser.password = undefined;
+  res
+    .status(201)
+    .json({ message: "user created succsefully", payload: newUser });
+});
 
 let signin = asyncHandler(async (req, res) => {
   let founduser = req.founduser;
   if (founduser.provider === "google") {
     return res.status(400).json({
-      message: "This account was registered using Google login. Please login with Google.",
+      message:
+        "This account was registered using Google login. Please login with Google.",
     });
   }
 
@@ -37,36 +36,57 @@ let signin = asyncHandler(async (req, res) => {
   }
 
   if (!founduser.isConfirmed) {
-    return res.status(401).json({ message: "You cannot log in without verifying your email" });
+    return res
+      .status(401)
+      .json({ message: "You cannot log in without verifying your email" });
   }
 
   const secret_key = process.env.JWT_SECRET;
   const token = jwt.sign(
     { role: founduser.role, email: founduser.email, _id: founduser._id },
-    secret_key  );
+    secret_key,
+  );
 
+  const session_id = req.headers.session_id;
+  console.log(session_id)
+  // Cart Part
+  if(session_id){
+    const cart = await Cart.findOne({ session_id });
+    if (cart){
+      cart.user=founduser._id;
+      cart.expiresAt=undefined;
+      await cart.save();
+    }
+  }
+
+  founduser.password = undefined;
   res.json({ message: "Hello", data: founduser, token: token });
 });
 
+let emailVerification = asyncHandler(async (req, res) => {
+  let email = req.params.email;
+  const email_signature = process.env.EMAIL_Token;
+  let verifiedEmail = jwt.verify(
+    email,
+    email_signature,
+    async (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "Invalid Token" });
+      }
 
-let emailVerification =asyncHandler( async (req,res)=>{
-    let email = req.params.email ;
-    const email_signature = process.env.EMAIL_Token ;
-    let verifiedEmail = jwt.verify(email,email_signature,async(err ,decoded)=>{
-        if(err){
-            return res.status(401).json({message: "Invalid Token"})
-        }
-
-        await userModel.findOneAndUpdate({email:decoded},{isConfirmed:true}) ;
-        const __filename = fileURLToPath(import.meta.url);
-        const __dirname = path.dirname(__filename);
-        const htmlFilePath = path.join(__dirname, "..", process.env.HTML_FILE);
-        const htmlContent = await fs.promises.readFile(htmlFilePath, "utf-8");
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(htmlContent);
-
-    })
-})
+      await userModel.findOneAndUpdate(
+        { email: decoded },
+        { isConfirmed: true },
+      );
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+      const htmlFilePath = path.join(__dirname, "..", process.env.HTML_FILE);
+      const htmlContent = await fs.promises.readFile(htmlFilePath, "utf-8");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(htmlContent);
+    },
+  );
+});
 const googleCallback = (req, res) => {
   const token = jwt.sign(
     {
@@ -74,12 +94,14 @@ const googleCallback = (req, res) => {
       role: req.user.role,
       email: req.user.email,
     },
-    process.env.JWT_SECRET  );
+    process.env.JWT_SECRET,
+  );
 
   res.json({
-    message: "Google login success",token,user: req.user,
+    message: "Google login success",
+    token,
+    user: req.user,
   });
 };
 
-
-export {signup , signin ,emailVerification,googleCallback}
+export { signup, signin, emailVerification, googleCallback };
